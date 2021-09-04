64074bd619bf2d45647ee94910dc08b4f2f3d9fc
stmidi613 <75884976+stmidi613@users.noreply.github.com>
2021-09-04 17:06:47 +0900

    started working on the vertical function.

--- word-search.js
+++ word-search.js
@@ -5,7 +5,8 @@ window.onload = function(){
     let words = ["time", "happy", "its"];
     let word = "";
     let wordArray = words.map(eachWord => eachWord.split(""));
-    
+    let newArray = [];
+
     let arr = [
         [0, 0, 0, 0, 0, 0, 0, 0],
         [1, 1, 1, 1, 1, 1, 1, 1],
@@ -14,19 +15,35 @@ window.onload = function(){
         [4, 4, 4, 4, 4, 4, 4, 4],
         [5, 5, 5, 5, 5, 5, 5, 5],
         [6, 6, 6, 6, 6, 6, 6, 6],
-        [7, 7, 7, 7, 7, 7, 7, 7]
+        [7, 7, "m", 7, 7, 7, 7, 7]
     ]
 //This function checks to see if the word can fit inside the puzzle.  
 //We are assuming the crossword is a perfect square.
     function canFit(word){
         return arr[0].length > word.length;
     }
+//This finds all the places the word can fit horizontally.
+    function canFitHorizontally(word){
+        return arr.map((num, i) => 
+            num.map((eachnum, index) => arr[0].length - word.length >= index 
+                                        && num.slice(index, index + word.length)
+                .every(item => /\d+/.test(item)) ? newArray.push([i, index, "horizontal"]) : ""
+                )
+            );
+    }
+//This finds all the places the word can fit vertically.
+    function canFitVertically(word){
+        return arr.map((num, i) => num);
+    }
 //returns an array which contains objects
     function allPossiblePositions(word){
         if(canFit(word)){
-           return arr.map(num => num.map((eachnum, index) => arr[0].length - word.length >= index ? num.slice(index, index + word.length) : ""));
+           return arr.map(num => num.filter((eachnum, index) => arr[0].length - word.length >= index && num.slice(index, index + word.length).every(item => /[0-9]/.test(item))));
         }
     }
     let test = arr.map(num => num.map(eachnum => eachnum));
-    console.log(allPossiblePositions("time"))    
+    console.log(canFitHorizontally("time"))
+    console.log(newArray)
+    console.log(arr[7][2])
+    console.log(arr.length)   
 }
