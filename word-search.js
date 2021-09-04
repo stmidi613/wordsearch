c94e93c82ceb0c9f97cbb936554db6b8e17619dc
stmidi613 <75884976+stmidi613@users.noreply.github.com>
2021-09-04 15:07:43 +0900

    started the new model for the WordSearch

--- word-search.js
+++ word-search.js
@@ -5,92 +5,28 @@ window.onload = function(){
     let words = ["time", "happy", "its"];
     let word = "";
     let wordArray = words.map(eachWord => eachWord.split(""));
-    const columns = document.querySelectorAll('tr');
-    const cells = document.querySelectorAll('td');
-
-    cells.forEach(cell => {
-        cell.innerText =  ALPHA[Math.floor((Math.random() * 26))];
-    })
     
-    function placeVertically(word){
-        const startingCell = cells[Math.floor(Math.random() * cells.length)];
-        const cellCoordinatesRow = startingCell.parentElement.rowIndex;
-        const cellCoordinatesColumn = startingCell.cellIndex;
-        return word.forEach((letter, index) => 
-        cellCoordinatesColumn + word.length <= columns.length ?  
-        columns[cellCoordinatesRow].cells[cellCoordinatesColumn + index].innerText = letter
-        : columns[cellCoordinatesRow].cells[cellCoordinatesColumn - index].innerText = letter);
+    let arr = [
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1],
+        [2, 2, 2, 2, 2, 2, 2, 2],
+        [3, 3, 3, 3, 3, 3, 3, 3],
+        [4, 4, 4, 4, 4, 4, 4, 4],
+        [5, 5, 5, 5, 5, 5, 5, 5],
+        [6, 6, 6, 6, 6, 6, 6, 6],
+        [7, 7, 7, 7, 7, 7, 7, 7]
+    ]
+//This function checks to see if the word can fit inside the puzzle.  
+//We are assuming the crossword is a perfect square.
+    function canFit(word){
+        return arr[0].length > word.length;
     }
-    //The columns.length is used because it is assumed the number of columns and rows are equal.
-    function placeHorizontally(word){
-        const startingCell = cells[Math.floor(Math.random() * cells.length)];
-        const cellCoordinatesRow = startingCell.parentElement.rowIndex;
-        const cellCoordinatesColumn = startingCell.cellIndex;
-        return word.forEach((letter, index) => 
-        cellCoordinatesRow + word.length <= columns.length ?
-        columns[cellCoordinatesRow + index].cells[cellCoordinatesColumn].innerText = letter
-        : columns[cellCoordinatesRow - index].cells[cellCoordinatesColumn].innerText = letter);
+//returns an array which contains objects
+    function allPossiblePositions(word){
+        if(canFit(word)){
+           return arr.map(num => num.map((eachnum, index) => arr[0].length - word.length >= index ? num.slice(index, index + word.length) : ""));
+        }
     }
-
-    function placeDiagonally(word){
-        const startingCell = cells[Math.floor(Math.random() * cells.length)];
-        const cellCoordinatesRow = startingCell.parentElement.rowIndex;
-        const cellCoordinatesColumn = startingCell.cellIndex;
-        return word.forEach((letter, index) => 
-        //Starting cell is in the top left of graph.
-        cellCoordinatesColumn + word.length <= columns.length &&
-        cellCoordinatesRow + word.length <= columns.length ?
-        columns[cellCoordinatesRow + index].cells[cellCoordinatesColumn + index].innerText = letter :
-        //Starting cell is in the top right of graph.
-        cellCoordinatesColumn + word.length >= columns.length &&
-        cellCoordinatesRow + word.length <= columns.length ?
-        columns[cellCoordinatesRow + index].cells[cellCoordinatesColumn - index].innerText = letter :
-        //Starting cell is in the bottom left of graph.
-        cellCoordinatesColumn + word.length <= columns.length &&
-        cellCoordinatesRow + word.length >= columns.length ?
-        columns[cellCoordinatesRow - index].cells[cellCoordinatesColumn + index].innerText = letter :
-        //Staring cell is in the bottom right of graph.
-        columns[cellCoordinatesRow - index].cells[cellCoordinatesColumn - index].innerText = letter);
-        }
-        
-        wordArray.forEach((word) => {
-            const columns = document.querySelectorAll('tr');
-            const cells = document.querySelectorAll('td');
-            const arr = [];
-            const possiblePlaces = findPlaces(word);
-            function findPlaces(word){
-                //word.forEach((letter, index) => {
-                cells.forEach(cell => {
-                    cell.cellIndex + word.length <= columns.length &&
-                    word.forEach((letter, index) => {
-                        arr.push(cells[cell.cellIndex + index]);
-                        arr.filter(cellStatus => cellStatus.dataset.status === "open")
-                        //arr.push([cell.cellIndex, cell.parentElement.rowIndex, "horizontal"]) : ""
-                        console.log(cells[cell.cellIndex])
-                        });
-                        console.log(arr)
-                    });
-                };
-            });
-    
-        /*function wordPlacement(word){
-    switch(Math.floor(Math.random() * 3)){
-        case 0:
-            placeVertically(word)
-            break;
-        case 1:
-            placeHorizontally(word)
-            break;
-        case 2:
-            placeDiagonally(word)
-            break;
-        }
-    }*/
-    
-    /*document.getElementById("answers").onclick = function(){
-        startingCell.style.borderColor = "red";
-    }*/
-
-    //return wordArray.forEach(word => wordPlacement(word));
+    let test = arr.map(num => num.map(eachnum => eachnum));
+    console.log(allPossiblePositions("time"))    
 }
-
