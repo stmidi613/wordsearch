55e5c5914177aaa91cddbdf5c0a1eb3d6a7e62c7
stmidi613 <75884976+stmidi613@users.noreply.github.com>
2021-09-20 17:12:31 +0900

    worked on the diagonal function but could not finish it properly.  Will continue next time.

--- word-search.js
+++ word-search.js
@@ -7,16 +7,18 @@ window.onload = function(){
     let wordArray = words.map(eachWord => eachWord.split(""));
     let newArray = [];
     let subArray = [];
+    let subArray2 = [];
+    let fullArr = [];
 
     let arr = [
         [0, 0, 0, 0, "t", 0, 0, 0],
         [1, 1, 1, 1, 1, 1, 1, 1],
         [2, 2, 2, 2, 2, 2, 2, 2],
         [3, 3, 3, 3, 3, 3, 3, 3],
-        [4, 4, 4, 4, 4, 4, 4, 4],
+        [4, "t", 4, 4, 4, 4, 4, 4],
         [5, 5, 5, 5, 5, 5, 5, 5],
         [6, 6, 6, 6, 6, 6, 6, 6],
-        [7, 7, 7, 7, 7, 7, 7, 7]
+        [7, 7, 7, 7, 7, 7, 7, "t"]
     ]
 //This function checks to see if the word can fit inside the puzzle.  
 //We are assuming the crossword is a perfect square.
@@ -45,32 +47,34 @@ window.onload = function(){
         );
     }
 //This finds all the places the word can fit diagonally.
-/*function canFitDiagonally(word){
+function canFitDiagonally(word){
     subArray = arr.map((num, i) => 
-                num.map((eachNum, index) => 
-                arr[i + 1][index])      
+                num.map((eachNum, index) => i + index < arr.length ? 
+                arr[index][index + i] : "")
     );
-    return subArray.map((num, i) => 
+    subArray2 = arr.map((num, i) => 
+    num.map((eachNum, index) => i + index < arr.length ? 
+    arr[index + i][index] : "") 
+    );
+    fullArr = subArray.concat(subArray2).slice(1).map(arr => arr.filter(num => /\w+/.test(num)));
+        fullArr.map((num, i) => 
             num.map((eachnum, index) => arr[0].length - word.length >= index 
                     && num.slice(index, index + word.length)
                 .every(item => /\d+/.test(item) || item === word[index]) ? 
-                newArray.push([index, i, "diagonal"]) : "")
+                newArray.push([num[0], index, "diagonal"]) : "")
     );
-}*/
+}
 //returns an array which contains objects
-    function allPossiblePositions(word){
+    /*function allPossiblePositions(word){
         if(canFit(word)){
            return arr.map(num => num.filter((eachnum, index) => arr[0].length - word.length >= index && num.slice(index, index + word.length).every(item => /[0-9]/.test(item))));
         }
-    }
+    }*/
     let test = arr.map(num => num.map(eachnum => eachnum));
     console.log(canFitHorizontally("time"))
     console.log(newArray)
     console.log(canFitVertically("time"))
-    //console.log(canFitDiagonally("time"))
-
-    document.getElementById("puzzle").insertAdjacentHTML("beforeend", 
-        `<tr>${`<td></td>`}</tr>`)
-        //arr.map(eachArr => <td>${eachArr.map(item => <tr>${item}</tr>)}</td>);  
+    console.log(canFitDiagonally("time"))
+    console.log(fullArr)
 }
 
